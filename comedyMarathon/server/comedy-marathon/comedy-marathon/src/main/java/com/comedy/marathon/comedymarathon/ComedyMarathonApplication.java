10604124949e6b7b945763ca2ef278560b7e4bb7
CPJ-N <nischalcharles@outlook.com>
2020-06-07 00:47:54 -0400

    added a new spring boot application through spring.io

---
+++ comedyMarathon/server/comedy-marathon/comedy-marathon/src/main/java/com/comedy/marathon/comedymarathon/ComedyMarathonApplication.java
@@ -0,0 +1,13 @@
+package com.comedy.marathon.comedymarathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComedyMarathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComedyMarathonApplication.class, args);
+	}
+
+}
---
+++ comedyMarathon/server/comedy-marathon/comedy-marathon/src/test/java/com/comedy/marathon/comedymarathon/ComedyMarathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comedy.marathon.comedymarathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComedyMarathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
